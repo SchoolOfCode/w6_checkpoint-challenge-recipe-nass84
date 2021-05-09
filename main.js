8f1fd92f0e5c65eaf895391d3be032a05969ca37
Becki Floyd <beckifloyd@gmail.com>
2021-05-09 17:51:44 +0100

    Added CSS

--- main.js
+++ main.js
@@ -11,8 +11,39 @@ function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
 
+// async function fetchRecipe(food) {
+//   //--- write your code below ---
+//   // remove text from recipe label
+//   let firstRecipe = document.getElementById("recipe-label");
+//   firstRecipe.innerText = "";
+//   const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`;
+//   const response = await fetch(requestUrl);
+//   const data = await response.json();
+//   console.log(data);
+//   const recipe = data.hits[0];
+//   // Change name of receipe
+//   let recipeName = document.getElementById("name-of-recipe");
+//   recipeName.innerText = recipe.recipe.label;
+//   // Link to get recipe
+//   firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
+//   firstRecipe.href = recipe.recipe.url;
+//     // // add image
+//   let firstRecipeImage = document.getElementById("recipe-picture");
+//   firstRecipeImage.src = recipe.recipe.image;
+// // add ingredients
+// let ingredients = document.getElementById("ingredients");
+// ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]} and ${recipe.recipe.ingredientLines[2]}`;
+
+//   //--- write your code above ---
+// }
+
+// - Can you have more than one recipe appear on the page with the information so people can pick from a selection?
+
 async function fetchRecipe(food) {
   //--- write your code below ---
+  // hide picture
+  let questionImage = document.getElementById("questionImage");
+questionImage.classList.add("hide");
   // remove text from recipe label
   let firstRecipe = document.getElementById("recipe-label");
   firstRecipe.innerText = "";
@@ -25,17 +56,34 @@ async function fetchRecipe(food) {
   let recipeName = document.getElementById("name-of-recipe");
   recipeName.innerText = recipe.recipe.label;
   // Link to get recipe
-  firstRecipe.innerText = `Click here for ${recipe.recipe.label} recipe`;
+  firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
   firstRecipe.href = recipe.recipe.url;
-  // find image in object
+    // // add image
   let firstRecipeImage = document.getElementById("recipe-picture");
   firstRecipeImage.src = recipe.recipe.image;
-  // // add image
-  document.getElementById("receipe-picture").appendChild(recipeImage);
+// add ingredients
+let ingredients = document.getElementById("ingredients");
+ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]} and ${recipe.recipe.ingredientLines[2]}`;
 
-  //--- write your code above ---
+  //--- second Receipe---
+  const recipe2 = data.hits[1];
+  // Change name of receipe
+  let recipeName2 = document.getElementById("name-of-recipe2");
+  recipeName2.innerText = recipe2.recipe.label;
+  // Link to get recipe
+  let secondRecipe = document.getElementById("recipe-label2");
+  secondRecipe.innerText = `Click here for full ${recipe2.recipe.label} recipe`;
+  secondRecipe.href = recipe2.recipe.url;
+    // // add image
+  let secondRecipeImage = document.getElementById("recipe-picture2");
+  secondRecipeImage.src = recipe2.recipe.image;
+// add ingredients
+let ingredients2 = document.getElementById("ingredients2");
+ingredients2.innerText = `Main Ingredients are ${recipe2.recipe.ingredientLines[0]} and ${recipe2.recipe.ingredientLines[1]} and ${recipe2.recipe.ingredientLines[2]}`;
+
 }
 
-// - Can you add images, ingredients or other information to your page?
-// - Can you have more than one recipe appear on the page with the information so people can pick from a selection?
+
+
+
 // - Add a CSS file and style your page appropriately.
