015a6962483e451bbd79b7768afeaf621c8d3803
Becki Floyd <beckifloyd@gmail.com>
2021-05-10 13:34:34 +0100

    Research and to do list MD added

--- main.js
+++ main.js
@@ -41,9 +41,6 @@ function handleFoodChange() {
 
 async function fetchRecipe(food) {
   //--- write your code below ---
-  // hide picture
-  let questionImage = document.getElementById("questionImage");
-questionImage.classList.add("hide");
   // remove text from recipe label
   let firstRecipe = document.getElementById("recipe-label");
   firstRecipe.innerText = "";
@@ -52,19 +49,22 @@ questionImage.classList.add("hide");
   const data = await response.json();
   console.log(data);
   const recipe = data.hits[0];
+  // hide picture
+  let questionImage = document.getElementById("questionImage");
+  questionImage.classList.add("hide");
   // Change name of receipe
   let recipeName = document.getElementById("name-of-recipe");
   recipeName.innerText = recipe.recipe.label;
   // Link to get recipe
   firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
   firstRecipe.href = recipe.recipe.url;
-    // // add image
+  // // add image
   let firstRecipeImage = document.getElementById("recipe-picture");
   firstRecipeImage.src = recipe.recipe.image;
-  firstRecipeImage.alt="picture of cooked receipe"
-// add ingredients
-let ingredients = document.getElementById("ingredients");
-ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]} and ${recipe.recipe.ingredientLines[2]}`;
+  firstRecipeImage.alt = "picture of cooked receipe";
+  // add ingredients
+  let ingredients = document.getElementById("ingredients");
+  ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]} and ${recipe.recipe.ingredientLines[2]}`;
 
   //--- second Receipe---
   const recipe2 = data.hits[1];
@@ -75,17 +75,13 @@ ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]
   let secondRecipe = document.getElementById("recipe-label2");
   secondRecipe.innerText = `Click here for full ${recipe2.recipe.label} recipe`;
   secondRecipe.href = recipe2.recipe.url;
-    // // add image
+  // // add image
   let secondRecipeImage = document.getElementById("recipe-picture2");
   secondRecipeImage.src = recipe2.recipe.image;
-  secondRecipeImage.alt="picture of cooked receipe"
-// add ingredients
-let ingredients2 = document.getElementById("ingredients2");
-ingredients2.innerText = `Main Ingredients are ${recipe2.recipe.ingredientLines[0]} and ${recipe2.recipe.ingredientLines[1]} and ${recipe2.recipe.ingredientLines[2]}`;
-
+  secondRecipeImage.alt = "picture of cooked receipe";
+  // add ingredients
+  let ingredients2 = document.getElementById("ingredients2");
+  ingredients2.innerText = `Main Ingredients are ${recipe2.recipe.ingredientLines[0]} and ${recipe2.recipe.ingredientLines[1]} and ${recipe2.recipe.ingredientLines[2]}`;
 }
-
-
-
 
 // - Add a CSS file and style your page appropriately.
---
+++ websiteResearch.md
@@ -0,0 +1,103 @@
+# Recipe Websites Research 
+
+## To do 
+- [ ] Pick Name
+- [ ] Make Logo
+- [ ] Add colour behind search bar
+- [ ] Match the font color to the logo 
+- [ ] Add border radius to pictures
+- [ ] Add big image for first result and smaller ones underneath 
+- [ ] Open up recipe in a new tab so user doesnt navigate from page
+- [ ] Add more recipes
+- [ ] Pick random receipe button  
+- [ ] Add animations when you click on picture
+
+
+## Website
+Link:     [Tescos](https://realfood.tesco.com/what-can-i-make-with.html)
+ 
+### Pros
+* Long list of recipes
+* Enter multiple ingredients
+* Drop down box after you start typing 
+* All ingredients included
+* Time taken to cook 
+* List underneath makes it easy to read
+
+### Cons
+* Minimum of 3 ingredients
+* White and grey design looks dull 
+
+## Website
+Link:     [Supercook](https://www.supercook.com/#/recipes)
+ 
+### Pros
+* Can search for individual recipes
+* Tick boxs for all the ingredients 
+* Choice to type in the ingredients
+* Voice search on app for ingredients
+* Works out what you can make with what you have
+* Like the border radius on the pictures
+* Multiple recipes on each search
+
+
+### Cons
+* Slow website
+* Design is boring, white with abit of pink 
+
+
+## Website
+Link:     [Best Recipe Finder](https://bestrecipefinder.com/)
+ 
+### Pros
+* Lots of selection of recipes
+* Text is easy to read at the top
+* Has a search bar to search for individual recipes
+
+### Cons
+* Recipes are random to start off with 
+* Design is boring, white with abit of green 
+
+## Website
+Link:     [Lexis Clean Living](https://lexiscleankitchen.com/recipe-finder/)
+ 
+### Pros
+* Like the logo at top of page
+* Search by Category is in words and clear to read and click on 
+* Like that the font colour matches the logo 
+
+### Cons
+* Pictures dont have titles under them so unclear what they are
+* Pictures are squares so dull  
+* Search bar doesnt work
+
+## Website
+Link:     [Bishs Dishes](https://bishsdishes.wordpress.com/recipe-finder/
+)
+ 
+### Pros
+* Like the picture of ingredients
+* Recipes are on the same website
+
+### Cons
+* Pictures dont have titles under them so unclear what they are
+* Pictures are squares so dull  
+* Search bar doesnt work
+
+## Website
+Link:     [Beef Whats for Dinner](
+https://www.beefitswhatsfordinner.com/recipes
+)
+ 
+### Pros
+* Love the text over the pictures, black background with white writing stands out
+* When you hover over the writing it changes colour and the picture changes colour
+* Has time to make, calories and protein on large website, hides it when smaller 
+* Love the search bar on top of a coloured background
+
+### Cons
+* Beef specific 
+* Search bar doesnt work 
+
+
+
