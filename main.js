cd721902f836e310659ab3530763719669817e90
Becki Floyd <beckifloyd@gmail.com>
2021-05-11 20:49:20 +0100

    Added API information by JavaScript

--- main.js
+++ main.js
@@ -11,96 +11,85 @@ function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
 
-// async function fetchRecipe(food) {
-//   //--- write your code below ---
-//   // remove text from recipe label
-//   let firstRecipe = document.getElementById("recipe-label");
-//   firstRecipe.innerText = "";
-//   const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`;
-//   const response = await fetch(requestUrl);
-//   const data = await response.json();
-//   console.log(data);
-//   const recipe = data.hits[0];
-//   // Change name of receipe
-//   let recipeName = document.getElementById("name-of-recipe");
-//   recipeName.innerText = recipe.recipe.label;
-//   // Link to get recipe
-//   firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
-//   firstRecipe.href = recipe.recipe.url;
-//     // // add image
-//   let firstRecipeImage = document.getElementById("recipe-picture");
-//   firstRecipeImage.src = recipe.recipe.image;
-// // add ingredients
-// let ingredients = document.getElementById("ingredients");
-// ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]} and ${recipe.recipe.ingredientLines[2]}`;
-
-//   //--- write your code above ---
-// }
-
-// - Can you have more than one recipe appear on the page with the information so people can pick from a selection?
+// add h2
+// add h2 class="name" id="name-of-recipe"
+// add image class="image" id="recipe-picture" id="image-website" href="" target="_blank"
+// add <p class="information" id="ingredients"></p>
+// add br
+// add <p class="information" id="meal-type"></p>
+//  <br>
+// add  <a class="link" id="recipe-label" target="_blank"></a>
 
 async function fetchRecipe(food) {
-  //--- write your code below ---
-  // remove text from recipe label
-  let firstRecipe = document.getElementById("recipe-label");
-  firstRecipe.innerText = "";
   const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`;
   const response = await fetch(requestUrl);
   const data = await response.json();
-  console.log(data);
   const recipe = data.hits[0];
-  // hide picture
-  let logoImage = document.getElementById("logoImage");
-  logoImage.classList.add("hide");
-  // hide logo
-  let logo = document.getElementById("logo");
-  logo.classList.add("hide");
-  //First Recipe
-  // Change name of receipe
-  let recipeName = document.getElementById("name-of-recipe");
-  recipeName.innerText = recipe.recipe.label;
-  // Link to get recipe
-  firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
-  firstRecipe.href = recipe.recipe.url;
-  // // add image
-  let firstRecipeImage = document.getElementById("recipe-picture");
-  firstRecipeImage.src = recipe.recipe.image;
-  firstRecipeImage.alt = "picture of cooked receipe";
-  // image to website
-  let firstImageWebsite = document.getElementById("image-website");
-  firstImageWebsite.href = recipe.recipe.url;
-
-  // add ingredients
-  let ingredients = document.getElementById("ingredients");
-  ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]}`;
-  // add meal type
-  let mealType = document.getElementById("meal-type");
-  mealType.innerText = ` ${recipe.recipe.dietLabels}`;
-
-  //--- second Receipe---
-  const recipe2 = data.hits[1];
-  // Change name of receipe
-  let recipeName2 = document.getElementById("name-of-recipe2");
-  recipeName2.innerText = recipe2.recipe.label;
-  // Link to get recipe
-  let secondRecipe = document.getElementById("recipe-label2");
-  secondRecipe.innerText = `Click here for full ${recipe2.recipe.label} recipe`;
-  secondRecipe.href = recipe2.recipe.url;
-  // // add image
-  let secondRecipeImage = document.getElementById("recipe-picture2");
-  secondRecipeImage.src = recipe2.recipe.image;
-  secondRecipeImage.alt = "picture of cooked receipe";
-  // image to website
-  let secondImageWebsite = document.getElementById("image-website2");
-  secondImageWebsite.href = recipe2.recipe.url;
-  // add ingredients
-  let ingredients2 = document.getElementById("ingredients2");
-  ingredients2.innerText = `Main Ingredients are ${recipe2.recipe.ingredientLines[0]} and ${recipe2.recipe.ingredientLines[1]}`;
-  // add meal type
-  let mealType2 = document.getElementById("meal-type2");
-  mealType2.innerText = ` ${recipe2.recipe.dietLabels}`;
+  console.log(recipe);
+  addRecipetoHTML(recipe.recipe);
 }
 
-// - Add a CSS file and style your page appropriately.
+function addRecipetoHTML(recipe) {
+   
+  // hide image
+  let logoImage = document.getElementById("logoImage");
+  logoImage.classList.add("hide");
+   // hide logo
+   let logo = document.getElementById("logo");
+   logo.classList.add("hide");
+ // on click clear the section 
+  let section = document.getElementById("recipe-section");
+  section.innerHTML = ""
+     // Create H2
+  let h2 = document.createElement("h2");
+  section.setAttribute("id", "recipe-section");
+  section.setAttribute("class", "recipe-item");
+  section.appendChild(h2);
+  // Add name of recipe to h2
+  let nameOfRecipe = recipe.label;
+  h2.innerText = nameOfRecipe;
+  h2.setAttribute("id", "name-of-recipe");
+  h2.setAttribute("class", "name");
+  //create a tag
+  let aTag = document.createElement("a");
+  aTag.setAttribute("href", recipe.url);
+  aTag.setAttribute("target", "_blank");
+  aTag.setAttribute("id", "image-website");
+  h2.appendChild(aTag);
+  // Show Image
+  let image = document.createElement("img");
+  image.setAttribute("src", recipe.image);
+  image.setAttribute("alt", "picture of cooked receipe");
+  image.setAttribute("id", "recipe-picture");
+  image.setAttribute("class", "image");
+  aTag.appendChild(image);
 
-// refresh page
+  // create p tag
+  // add ingredients
+  let ingredientText = document.createElement("p");
+  ingredientText.innerText = `Main Ingredients are ${recipe.ingredientLines[0]} and ${recipe.ingredientLines[1]}`;
+  ingredientText.setAttribute("id", "ingredients");
+  ingredientText.setAttribute("class", "information");
+  h2.appendChild(ingredientText);
+
+  // add meal type
+  let mealTypeText = document.createElement("p");
+  mealTypeText.innerText = recipe.dietLabels;
+  mealTypeText.setAttribute("id", "meal-type");
+  mealTypeText.setAttribute("class", "information");
+  ingredientText.appendChild(mealTypeText);
+  //Create break
+  let breakTag = document.createElement("br");
+  mealTypeText.appendChild(breakTag);
+  // Link to get recipe
+  let aTag2 = document.createElement("a");
+  aTag2.setAttribute("href", recipe.url);
+  aTag2.setAttribute("target", "_blank");
+  aTag2.setAttribute("id", "recipe-label");
+  aTag2.setAttribute("class", "link");
+  aTag2.innerText = `Click here for full ${recipe.label} recipe`;
+  mealTypeText.appendChild(aTag2);
+
+}
+
+
---
+++ old.js
@@ -0,0 +1,106 @@
+const APP_ID = "d8fd0460";
+const APP_KEY = "37050a008e4132375bf91f4f49a78d3e";
+
+let foodToSearch = null;
+
+function handleRecipeClick() {
+  fetchRecipe(foodToSearch);
+}
+
+function handleFoodChange() {
+  foodToSearch = document.querySelector("#food-input").value;
+}
+
+// async function fetchRecipe(food) {
+//   //--- write your code below ---
+//   // remove text from recipe label
+//   let firstRecipe = document.getElementById("recipe-label");
+//   firstRecipe.innerText = "";
+//   const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`;
+//   const response = await fetch(requestUrl);
+//   const data = await response.json();
+//   console.log(data);
+//   const recipe = data.hits[0];
+//   // Change name of receipe
+//   let recipeName = document.getElementById("name-of-recipe");
+//   recipeName.innerText = recipe.recipe.label;
+//   // Link to get recipe
+//   firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
+//   firstRecipe.href = recipe.recipe.url;
+//     // // add image
+//   let firstRecipeImage = document.getElementById("recipe-picture");
+//   firstRecipeImage.src = recipe.recipe.image;
+// // add ingredients
+// let ingredients = document.getElementById("ingredients");
+// ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]} and ${recipe.recipe.ingredientLines[2]}`;
+
+//   //--- write your code above ---
+// }
+
+// - Can you have more than one recipe appear on the page with the information so people can pick from a selection?
+
+async function fetchRecipe(food) {
+  //--- write your code below ---
+  // remove text from recipe label
+  let firstRecipe = document.getElementById("recipe-label");
+  firstRecipe.innerText = "";
+  const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`;
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+  console.log(data);
+  const recipe = data.hits[0];
+  // hide picture
+  let logoImage = document.getElementById("logoImage");
+  logoImage.classList.add("hide");
+  // hide logo
+  let logo = document.getElementById("logo");
+  logo.classList.add("hide");
+  //First Recipe
+  // Change name of receipe
+  let recipeName = document.getElementById("name-of-recipe");
+  recipeName.innerText = recipe.recipe.label;
+  // Link to get recipe
+  firstRecipe.innerText = `Click here for full ${recipe.recipe.label} recipe`;
+  firstRecipe.href = recipe.recipe.url;
+  // // add image
+  let firstRecipeImage = document.getElementById("recipe-picture");
+  firstRecipeImage.src = recipe.recipe.image;
+  firstRecipeImage.alt = "picture of cooked receipe";
+  // image to website
+  let firstImageWebsite = document.getElementById("image-website");
+  firstImageWebsite.href = recipe.recipe.url;
+
+  // add ingredients
+  let ingredients = document.getElementById("ingredients");
+  ingredients.innerText = `Main Ingredients are ${recipe.recipe.ingredientLines[0]} and ${recipe.recipe.ingredientLines[1]}`;
+  // add meal type
+  let mealType = document.getElementById("meal-type");
+  mealType.innerText = ` ${recipe.recipe.dietLabels}`;
+
+  //--- second Receipe---
+  const recipe2 = data.hits[1];
+  // Change name of receipe
+  let recipeName2 = document.getElementById("name-of-recipe2");
+  recipeName2.innerText = recipe2.recipe.label;
+  // Link to get recipe
+  let secondRecipe = document.getElementById("recipe-label2");
+  secondRecipe.innerText = `Click here for full ${recipe2.recipe.label} recipe`;
+  secondRecipe.href = recipe2.recipe.url;
+  // // add image
+  let secondRecipeImage = document.getElementById("recipe-picture2");
+  secondRecipeImage.src = recipe2.recipe.image;
+  secondRecipeImage.alt = "picture of cooked receipe";
+  // image to website
+  let secondImageWebsite = document.getElementById("image-website2");
+  secondImageWebsite.href = recipe2.recipe.url;
+  // add ingredients
+  let ingredients2 = document.getElementById("ingredients2");
+  ingredients2.innerText = `Main Ingredients are ${recipe2.recipe.ingredientLines[0]} and ${recipe2.recipe.ingredientLines[1]}`;
+  // add meal type
+  let mealType2 = document.getElementById("meal-type2");
+  mealType2.innerText = ` ${recipe2.recipe.dietLabels}`;
+}
+
+// - Add a CSS file and style your page appropriately.
+
+// refresh page
